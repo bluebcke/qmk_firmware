6a5170b54bcbc55cd1d21574f9d5a929a570a88a
Brad Luebcke <bluebcke@gmail.com>
2023-05-21 18:13:12 -0400

    setup basic keyboard layout

---
+++ keyboards/keychron/k10_pro/ansi/rgb/keymaps/bluebcke/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// We want 5 layers.
+// The 2 Mac layers in the factory default. (layer 0-1, needs toggle switch)
+// The primary Windows layer. (layer 2)
+// The factory FN layer. (layer 3)
+// My CMD layer for emacs style and other commands/macros. (layer 4)
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
